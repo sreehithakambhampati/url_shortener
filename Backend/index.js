365f17fb32be818544189fb4dff8dde051dfd87e
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-06-24 12:14:42 +0530

    Updating cors

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.CORS_ORIGIN,
     credentials: true 
 }));
 app.use(attachUser)
